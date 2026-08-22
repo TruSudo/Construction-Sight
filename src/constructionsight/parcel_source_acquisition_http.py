46b4d58d966ea6a7bcb4f8747d48816ae84bce61
TruSudo <trutech67@gmail.com>
2026-08-22 00:24:44 -0700

    Bind ArcGIS probe transport to verified county hosts

--- src/constructionsight/parcel_source_acquisition_http.py
+++ src/constructionsight/parcel_source_acquisition_http.py
@@ -31,7 +31,7 @@ from constructionsight.parcel_source_verification_models import (
     ParcelSourceVerificationProfile,
 )
 
-_ARCGIS_ALLOWED_HOSTS = ("arcgis.com", "*.arcgis.com", "*.gov")
+_ARCGIS_ALLOWED_HOSTS = ("gis.countyofriverside.us", "services.arcgis.com")
 _ARCGIS_ALLOWED_PATH_PREFIXES = ("/arcgis/rest/services/", "/server/rest/services/")
 _ARCGIS_ACCEPTED_MEDIA_TYPES = ("application/json", "text/plain")
 
