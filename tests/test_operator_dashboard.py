f77b92dc3251449550cb4ef3ff5ed528c7eaa629
TruSudo <trutech67@gmail.com>
2026-09-24 19:30:46 -0700

    Sort operator dashboard integration imports

--- tests/test_operator_dashboard.py
+++ tests/test_operator_dashboard.py
@@ -34,6 +34,7 @@ from constructionsight.operator_dashboard import (
     build_workflow_snapshot,
     build_workflow_status_summary,
 )
+from constructionsight.operator_entity_index import build_entity_index
 from constructionsight.operator_results import build_result_ledger_snapshot
 from constructionsight.operator_services.ceqanet_persistence_service import (
     execute_authorized_ceqanet_write_plan,
@@ -42,7 +43,6 @@ from constructionsight.operator_source_candidate import (
     SourceRecordNotFound,
     build_source_candidate_preview,
 )
-from constructionsight.operator_entity_index import build_entity_index
 from constructionsight.operator_web import _parameters, create_handler
 from constructionsight.parcel_core_models import (
     ParcelCoreRecord,
